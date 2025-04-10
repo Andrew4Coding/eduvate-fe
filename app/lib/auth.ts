e4515ffb2143d7c54afb00cb41b10dcde9197958
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-10 20:30:45 +0800

    feat: add role to custom session

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { customSession } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -10,4 +11,50 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    plugins: [
+        customSession(async ({ user, session }) => {
+            const teacher = await prisma.teacher.findFirst({
+                where: {
+                    userId: user.id,
+                },
+                select: {
+                    id: true,
+                }
+            });
+
+            const student = await prisma.student.findFirst({
+                where: {
+                    userId: user.id,
+                },
+                select: {
+                    id: true,
+                }
+            });
+            const admin = await prisma.admin.findFirst({
+                where: {
+                    userId: user.id,
+                },
+                select: {
+                    id: true,
+                }
+            });
+
+            let role = ''
+            if (teacher) {
+                role = 'teacher'
+            } else if (student) {
+                role = 'student'
+            } else if (admin) {
+                role = 'admin'
+            }
+
+            return {
+                user: {
+                    ...user,
+                    role: role,
+                },
+                session
+            };
+        })
+    ]
 })

77377bfb83203ae2c232b10db0255471429198d1
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-10 20:59:14 +0800

    feat: add see profile page

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+import { customSessionClient } from "better-auth/client/plugins";
+import type { auth } from "~/lib/auth"; // Import the auth instance as a type
+
 
 export const authClient = createAuthClient({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://localhost:3000',
+    plugins: [customSessionClient<typeof auth>()],
 });
